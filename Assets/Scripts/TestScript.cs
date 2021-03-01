9eb8aa8876e711ba7e7cc6fd569f772d5da23225
Dantrix555 <suarezochoa98@gmail.com>
2021-03-01 15:48:33 -0500

    Package added

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DantrixCompany.Core;
+
+public class TestScript : MonoBehaviour
+{
+    private void Start()
+    {
+        EasyButtonsTest.SayHello();
+    }
+}
